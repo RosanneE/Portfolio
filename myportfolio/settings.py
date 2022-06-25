0b77b88010843370b33c24c00aeb76baecfa3d16
rosanne-a <anderson.rosanne4@gmail.com>
2022-06-25 14:15:49 -0400

    postreSQL update

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'myportfolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+ 'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'my_db',
+        'USER': 'hero',
+        'PASSWORD': 'why',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
